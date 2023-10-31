391c64e995068cc192d30179525defd1f8caa4ed
Luihza Marques <marquesluihza@gmail>
2023-10-30 20:19:27 -0400

    conceitos iniciais de js

---
+++ aula.10/index.js
@@ -0,0 +1,101 @@
+//****ORDEM DE FLUXO*****/
+
+    // function fn2(){
+    //     setTimeout(()=>{
+    //         console.log('oi');
+    //     });
+    // }
+
+    // function fn1(){
+    //     fn2();
+    // }
+
+    // function main(){
+    //     fn1();
+    // }
+
+    // main();
+    // console.log("tchau mundo");
+
+// ******tipos de dados *****
+
+    // var valor1;
+
+    // let valor2;
+
+    // const valor3 = []; //uma constante DEVE ser inicializada
+    // valor3.push('a'); 
+
+
+    // function main(){
+    //     let valor2;
+    //     console.log(valor1);
+    //     console.log(valor2);
+    // }
+
+
+
+// ******Condicionais********************
+
+// const euclido = 1; 
+
+
+// function main(){
+
+//     if(euclido>2){
+//         console.log('erro');
+//     }
+//     else if(euclido ==1){
+//         console.log("euclido");
+//     }
+//     else{
+//         console.log('certo');
+//     }
+// }
+
+
+// const euclido = 1; 
+
+
+// function main(){
+
+//   if(euclido >2 && euclido <5){
+//      console.log('errado');}    
+
+
+// const euclido =3;
+
+// function main(){
+
+
+//     const numero =1;
+//     numero++;
+//     numero--;
+
+//     // && duas condições em um escopo
+//     // || duas condições em um escopo 
+//     // % resto da divisao a%b
+//     //
+
+// }
+
+// main();
+
+
+const euclido =3;
+
+function main(){
+
+
+    const verdadeiro = ' ';   //há diferença entre uma string vazia (é indefinido) e uma string com espaço 
+
+    if(!verdadeiro){   // !variavel "diferente de"
+        console.log('VERDADEIRO');
+    }
+    else{
+        console.log('FALSO');
+    }
+
+}
+
+main();
