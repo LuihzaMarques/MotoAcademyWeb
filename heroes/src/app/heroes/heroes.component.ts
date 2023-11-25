8d47875d94161d1402c30ca718681d2b475d81c6
Luihza Marques <marquesluihza@gmail>
2023-11-24 20:22:16 -0400

    heroes-part-2

--- heroes/src/app/heroes/heroes.component.ts
+++ heroes/src/app/heroes/heroes.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import {HEROES} from './mocks/Hero.mocks'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = []
+  selectedHero?: Hero;
+
+
+  constructor(private heroService: HeroService){}
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
+  onSelectHero(hero: Hero){
+    this.selectedHero = hero;   
+  }
+
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
 }
+
+// this ? selectHero é um atribuito, e para se ter acesso precisa do THIS
+// referenciando algo dentro de um contexto. 
+// conceitos de POO 
---
+++ heroes/src/app/heroes/mocks/Hero.mocks.ts
@@ -0,0 +1,14 @@
+import { Hero } from "src/app/Hero";
+
+export const HEROES: Hero[] = [
+
+    {id: 1, name: "Batman"},
+    {id: 2, name: "Superman"},
+    {id: 3, name: "Wolverine"},
+    {id: 4, name: "Deadpool"},
+
+]   
+
+//criando uma constante heros com uma tipagem hero
+//mock usada para testes futuros
+//artigos de conteúdo para teste - criado fora a parte - volta um nível. 
