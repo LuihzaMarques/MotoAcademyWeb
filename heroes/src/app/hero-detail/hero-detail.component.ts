02150b6ab6521e98aeb17c1779a924e388eace45
Luihza Marques <marquesluihza@gmail>
2023-11-29 20:24:50 -0400

    heroes part.3

--- heroes/src/app/hero-detail/hero-detail.component.ts
+++ heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,11 +11,29 @@ import { Hero } from '../Hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+   
+  hero: Hero | undefined
+ // hero: Hero | undefined
 
-  @Input() hero?: Hero
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+    ){}
+
 
   ngOnInit(): void {
-    
+    this.getHero();  //faz aparecer a descrição do personagem 
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack():void{
+    this.location.back()
   }
 
 }
--- heroes/src/app/hero.service.ts
+++ heroes/src/app/hero.service.ts
@@ -7,16 +7,21 @@ import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class HeroService {
-
-  constructor(private messageService: MessageService){}  
-
-  //estamos retornando com certeza um valor de uma lista de heroes. 
+  
+  constructor(private messageService: MessageService){}
   getHeroesMock():Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add("Hero Service: fetched heroes");
     return heroes
   }
+
+
+  getHero(id: number):Observable<Hero> {
+    const hero = HEROES.find(heroValue => heroValue.id === id)!;
+    this.messageService.add(`HeroService: Fetched hero id=${id}`)
+    return of(hero)
+  }
 }
-
-
